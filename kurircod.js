80d85cdef75cab4e83b5e2908787c71e12a95fc5
Heri <manorder123@gmail.com>
2021-03-13 14:24:12 +0700

    adding state change

--- kurircod.js
+++ kurircod.js
@@ -1,19 +1,25 @@
-form = document.getElementsByName('courier')[0]
 
-let kurir = {
-    "sicepat": 20,
-    "J&T": 20
-}
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        form = document.getElementsByName('courier')[0]
 
-let kurirkey = Object.keys(kurir)
-
-form.dataset.prev
-
-form.onchange = function(){
-    let key = form.dataset.prev
-    if(kurirkey.includes(key)){
-        myProduct["shipping"]["cod"]["percentage"] = kurir[key];
-        console.log('custom trigger cod ongkir');
+        let kurir = {
+            "sicepat": 20,
+            "J&T": 20
+        }
+        
+        let kurirkey = Object.keys(kurir)
+        
+        form.dataset.prev
+        
+        form.onchange = function(){
+            let key = form.dataset.prev
+            if(kurirkey.includes(key)){
+                myProduct["shipping"]["cod"]["percentage"] = kurir[key];
+                console.log('custom trigger cod ongkir');
+            }
+        
+        }
     }
+  };
 
-}
